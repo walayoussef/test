7057ca10592938a26f2a49f9a01c12a32b0c3dcf
walayoussef <youssef.wala@enis.tn>
2026-07-14 10:07:19 +0200

    hjjj

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+// firebaseConfig.js
+const firebaseConfig = {
+  apiKey: "AIzaSyAwI7v4kWZTmqPX8IH0PGdgHO3k66OFksI",
+  authDomain: "tri-dechets-pfe.firebaseapp.com",
+  databaseURL: "https://tri-dechets-pfe-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "tri-dechets-pfe",
+  storageBucket: "tri-dechets-pfe.appspot.com",
+  messagingSenderId: "72975863015",
+  appId: "1:72975863015:web:4ff61a7e7e7a92013b2642"
+};
+
+// Initialiser Firebase avec compat
+firebase.initializeApp(firebaseConfig);
